bc7768640d2e7f32b1966cdb7eacf025c72eb7e1
Sandip Samal <sandip.samal@childrens.harvard.edu>
2023-05-02 12:20:33 -0400

    version bump to 2.2.0

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings, MongoDsn
 
 class Settings(BaseSettings):
     pflink_mongodb: MongoDsn = 'mongodb://localhost:27017'
-    version: str = "2.1.9"
+    version: str = "2.2.0"
 
 
 settings = Settings()
