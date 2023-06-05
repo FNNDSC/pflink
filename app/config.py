d07e0d863cb916c21ef5f11da1568e8b55b8e406
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2023-06-05 13:01:49 -0400

    Version bump

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings, MongoDsn
 
 class Settings(BaseSettings):
     pflink_mongodb: MongoDsn = 'mongodb://localhost:27017'
-    version: str = "3.1.3"
+    version: str = "3.2.0"
 
 
 class Auth(BaseSettings):
