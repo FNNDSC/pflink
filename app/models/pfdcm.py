1860052486431427549a71523ce071e5baa01cc0
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2023-06-29 17:23:50 -0400

    merge and version bump to 3.5.2

--- app/models/pfdcm.py
+++ app/models/pfdcm.py
@@ -5,7 +5,7 @@ from typing import List
 class PfdcmQuerySchema(BaseModel):
     """This class represents a `pfdcm` service"""
     service_name: str = Field(..., title="A string representing the name of a `pfdcm` service", example="PFDCM")
-    service_address: str = Field(..., title="Service address of the `pfdcm` instance", example="http://localhost:4005")
+    service_address: str = Field(..., title="Service address of the `pfdcm` instance", example="http://localhost:4005/api/v1")
 
 
 class PfdcmCollectionResponseModel(BaseModel):
