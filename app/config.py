17bc3f0b4ab6ca4f8c7a794b9a5e09d2ad8467aa
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2024-03-28 12:30:27 -0400

    version bump to retry build

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings, MongoDsn
 
 class Settings(BaseSettings):
     pflink_mongodb: MongoDsn = 'mongodb://localhost:27017'
-    version: str = "4.0.0"
+    version: str = "4.0.1"
     mongo_username: str = "admin"
     mongo_password: str = "admin"
     log_level: str = "DEBUG"
